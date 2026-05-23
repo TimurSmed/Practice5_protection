7444ba609330d48df86d856476f35d5d5832fcf9
TimurSmed <timur@example.com>
2026-05-23 15:29:41 +0000

    Add test for /metrics endpoint

--- test_main.py
+++ test_main.py
@@ -8,3 +8,10 @@ def test_time_not_zero():
     assert response.status_code == 200
     data = response.json()
     assert data["time"] != 0
+
+def test_metrics_count():
+    client.get("/time")
+    response = client.get("/metrics")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["count"] > 0
