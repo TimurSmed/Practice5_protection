2d95d77e81242b1982b43580fd41c04675002600
KamalovArtem <artik.kamalov08@gmail.com>
2026-05-23 14:43:00 +0000

    Add test for /time endpoint and CI workflow

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_time_not_zero():
+    response = client.get("/time")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["time"] != 0
